8079f4a117261d806dccf5d780c314147d6fb270
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-07 22:44:54 +0200

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -7,3 +7,11 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ('name', 'author')
+
+    
+    def clean_name(self):
+        name = self.changed_data['name']
+        if name.startswith('a'):
+            raise forms.ValidationError("Error: we don't want people whose name starts with a")
+        
+        return name
