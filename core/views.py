5d2903fde489f41ff3d81630740e4468afe23639
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-07 22:38:01 +0200

    Update views.py

--- core/views.py
+++ core/views.py
@@ -3,6 +3,14 @@ from core.forms import BookForm
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            context = {'form': BookForm()}
+            return render(request, 'partials/bookform.html',context)
+        context = {'form': form}
+        return render(request, 'partials/bookform.html',context)
     context = {
         'form': BookForm()
     }
